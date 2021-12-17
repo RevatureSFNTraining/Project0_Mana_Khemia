136e9145881f2898bc6a14058481c65e8dccf74e
payalvpatel <payal.patel@revature.net>
2021-12-17 13:45:22 -0500

    Edit CSS style

--- theFreshFeast/src/client/modules/my/headerClass/headerClass.js
+++ theFreshFeast/src/client/modules/my/headerClass/headerClass.js
@@ -4,12 +4,14 @@ import headerClass from './headerClass.html';
 import contactUs from '../contactUs/contactUs.html';
 import aboutUs from '../aboutUs/aboutUs.html';
 import homePage from '../homePage/homePage.html';
+import menuPage from '../menuPage/menuPage.html';
 
 export default class HeaderClass extends LightningElement{
     
     showContactUs = false;
     showAboutUs = false;
     showHomePage = true;
+    showMenuPage = false;
 
     render() {
         //console.log("the value:" + this.showContactUs + this.showAboutUs);
@@ -19,6 +21,7 @@ export default class HeaderClass extends LightningElement{
         this.showContactUs = true;
         this.showAboutUs = false;
         this.showHomePage = false;
+        this.showMenuPage = false;
 
         if(this.showContactUs){
             console.log("the value in handleClick if:" + this.showContactUs);
@@ -32,6 +35,7 @@ export default class HeaderClass extends LightningElement{
         this.showAboutUs = true;
         this.showContactUs = false;
         this.showHomePage = false;
+        this.showMenuPage = false;
 
         if(this.showAboutUs){
             console.log("the value in handleClick if:" + this.showAboutUs);
@@ -44,10 +48,25 @@ export default class HeaderClass extends LightningElement{
         this.showHomePage = true;
         this.showContactUs = false;
         this.showAboutUs = false;
+        this.showMenuPage = false;
 
         if(this.showHomePage){
             console.log("the value in home handleClick if:" + this.showHomePage);
             return homePage;
+        }
+            
+        return 0;
+    }
+
+    handleMenuClick(event){
+        this.showMenuPage = true;
+        this.showHomePage = false;
+        this.showContactUs = false;
+        this.showAboutUs = false;
+
+        if(this.showMenuPage){
+            console.log("the value in home handleClick if:" + this.showMenuPage);
+            return menuPage;
         }
             
         return 0;
