e9cb548e27e95862eab7777ff935d400b5736068
johnnieasibuo <johnnie.opoku@gmail.com>
2021-12-19 16:40:16 -0600

    started nav implementation

--- myapp/src/client/modules/my/nav/nav.js
+++ myapp/src/client/modules/my/nav/nav.js
@@ -1,6 +1,9 @@
 import { LightningElement } from 'lwc';
+import Opportunities from '../opportunities/opportunities.html';
 
 export default class Nav extends LightningElement {
-
     
+    handleOpport() {
+        return Opportunities;
+    }
 }
