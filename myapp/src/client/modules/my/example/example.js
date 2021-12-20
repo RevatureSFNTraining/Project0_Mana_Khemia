fcd10a396b491cfe40caf8ae58b66dd9745cecce
johnnieasibuo <johnnie.opoku@gmail.com>
2021-12-19 19:55:59 -0600

    homepage changes

--- myapp/src/client/modules/my/example/example.js
+++
@@ -1,6 +0,0 @@
-import { LightningElement } from 'lwc';
-
-export default class Example extends NavigationMixin(LightningElement) 
-{
-    
-}
--- myapp/src/client/modules/my/nav/nav.js
+++ myapp/src/client/modules/my/nav/nav.js
@@ -1,9 +1,6 @@
 import { LightningElement } from 'lwc';
-import Opportunities from '../opportunities/opportunities.html';
+
 
 export default class Nav extends LightningElement {
     
-    handleOpport() {
-        return Opportunities;
-    }
 }
