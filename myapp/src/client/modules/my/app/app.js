1615605b9435e0e8972121689ba5b6b547bba0fe
johnnieasibuo <johnnie.opoku@gmail.com>
2021-12-21 15:29:47 -0600

    finished functional  navbar

--- myapp/src/client/modules/my/app/app.js
+++ myapp/src/client/modules/my/app/app.js
@@ -1,5 +1,64 @@
 import { LightningElement, wire } from 'lwc';
 
 export default class App extends LightningElement {
+   body = "home"
+   body ="our company"
+
+   home = true;
+   ourcompany = false;
+   services = false;
+   opportunities = false;
+   contact = false;
+
+   handleNavChange(event) {
+    this.body = event.detail;
    
+    switch(this.body) {
+
+         case 'Home':
+            this.home = true;
+            this.ourcompany = false;
+            this.services = false;
+            this.opportunities = false;
+            this.contact = false;
+            break;
+   
+         case 'Our Company':
+            this.home = false;
+            this.ourcompany = true;
+            this.services = false;
+            this.opportunities = false;
+            this.contact = false;
+         break;
+   
+
+         case 'Services':
+            this.home = false;
+            this.ourcompany = false;
+            this.services = true;
+            this.opportunities = false;
+            this.contact = false;
+         break;
+   
+
+         case 'Opportunities':
+            this.home = false;
+            this.ourcompany = false;
+            this.services = false;
+            this.opportunities = true;
+            this.contact = false;
+         break;
+   
+         case 'Contact':
+            this.home = false;
+            this.ourcompany = false;
+            this.services = false;
+            this.opportunities = false;
+            this.contact = true;
+            break;
+
+   }
+
+}
+
 }
--- myapp/src/client/modules/my/nav/nav.js
+++ myapp/src/client/modules/my/nav/nav.js
@@ -2,5 +2,7 @@ import { LightningElement } from 'lwc';
 
 
 export default class Nav extends LightningElement {
-    
+    handleNavClick(event) {
+        this.dispatchEvent(new CustomEvent("navchange", {detail:event.target.name}));
+    }
 }
